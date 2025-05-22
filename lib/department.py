4087998a77e6eea4ca7cca217995c74a5927833e
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-05-22 23:51:48 +0300

    ORM properties

--- lib/department.py
+++ lib/department.py
@@ -1,8 +1,44 @@
 # lib/department.py
 from __init__ import CURSOR, CONN
 
-
 class Department:
+
+    # Dictionary of objects saved to the database.
+    all = {}
+
+    def __init__(self, name, location, id=None):
+        self.id = id
+        self.name = name
+        self.location = location
+
+    def __repr__(self):
+        return f"<Department {self.id}: {self.name}, {self.location}>"
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError(
+                "Name must be a non-empty string"
+            )
+
+    @property
+    def location(self):
+        return self._location
+
+    @location.setter
+    def location(self, location):
+        if isinstance(location, str) and len(location):
+            self._location = location
+        else:
+            raise ValueError(
+                "Location must be a non-empty string"
+            )
 
     # Dictionary of objects saved to the database.
     all = {}
